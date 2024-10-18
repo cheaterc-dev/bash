9a81781b5eac6f3df1d7830c824e8eba6ee8345a
cheaterc-dev <99823559+cheaterc-dev@users.noreply.github.com>
2024-10-18 17:24:32 +0300

    Update glpi.sh

--- GLPI/glpi.sh
+++ GLPI/glpi.sh
@@ -41,7 +41,7 @@ mysql -e "CREATE DATABASE glpi_db"
 # Create a new user
 mysql -e "CREATE USER 'glpi'@'localhost' IDENTIFIED BY '$SQLGLPIPWD'"
 # Grant privileges to the new user for the new database
-mysql -e "GRANT ALL PRIVILEGES ON glpi_db* TO 'glpi'@'localhost'"
+mysql -e "GRANT ALL PRIVILEGES ON glpi_db.* TO 'glpi'@'localhost'"
 # Reload privileges
 mysql -e "FLUSH PRIVILEGES"
 
